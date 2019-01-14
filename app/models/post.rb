f8971b197e2d1d9ab4e17953875973932778c198
teddylopez <tedmlopez@gmail.com>
2019-01-14 12:29:47 -0500

    Implement user connection to posts with tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   validates_presence_of :date, :rationale
+  belongs_to :user
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,8 +15,10 @@ describe 'navigate' do
   end
 
   describe 'creation' do
-    
+
     before do
+      user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Shred", last_name: "Lopez")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -27,10 +29,17 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User_Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User_Association")
     end
   end
 
