16f96bf71fd3eec0fea8888006ab5620d270dd2e
teddylopez <tedmlopez@gmail.com>
2019-01-15 18:52:09 -0500

    Edit post status functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,12 @@
 
 puts "1 user created"
 
+AdminUser.create(email: "admin@test.com", password: "password", password_confirmation: "password", first_name: "Shred", last_name: "Lopez")
+
+puts "1 admin user created"
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post}: Just because...", user: @user)
+  Post.create!(date: Date.today, rationale: "#{post}: Just because...", user_id: @user.id)
 end
 
 puts "100 posts created"
---
+++ spec/features/approval_workflow_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'navigate' do
+  before do
+    @admin = FactoryGirl.create(:admin_user)
+    login_as(@admin, :scope => :user)
+  end
+
+  describe 'edit' do
+    before do
+      @post = FactoryGirl.create(:post)
+    end
+
+    it 'has a status that can be edited on the form' do
+      visit edit_post_path(@post)
+      choose('post_status_approved')
+      click_on "Save"
+      expect(@post.reload.status).to eq('approved')
+    end
+  end
+end
