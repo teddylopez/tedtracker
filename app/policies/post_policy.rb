2743940d8b0c933928e8d8421c99de864964c4a3
Ted Lopez <ted.lopez@nationals.com>
2019-01-18 10:15:06 -0500

    Lock posts when approved by an admin

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,20 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || admin_types.include?(user.type) 
+    return true if record.approved? && admin?
+    return true if user_or_admin && !record.approved?
   end
+
+  private
+
+    def user_or_admin
+      record.user_id == user.id || admin?
+    end
+
+    def admin?
+      admin_types.include?(user.type)
+    end
+
+    def post_approved
+      record.approved?
+    end
 end
--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -25,5 +25,17 @@ describe 'navigate' do
       visit edit_post_path(@post)
       expect(page).to_not have_content('Approved')
     end
+
+    it 'should not be editable by post creator if status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+      expect(current_path).to eq(root_path)
+    end
+
   end
 end
