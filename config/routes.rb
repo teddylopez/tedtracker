ef7ef688c7eac78cf54714e0685799e6d9739e00
teddylopez <tedmlopez@gmail.com>
2019-01-19 12:47:46 -0500

    Style pending approvals on employee homepage and create confirmation action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy]
+  resources :audit_logs, except: [:new, :edit, :destroy] do
+    member do
+      get :confirm
+    end
+  end
   namespace :admin do
     resources :users
     resources :posts
