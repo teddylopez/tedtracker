a3126b65ac284ae14d041fcc40e0b927f759571d
teddylopez <tedmlopez@gmail.com>
2019-01-19 14:10:16 -0500

    Modify Devise initializer for production and compile assets.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'daae7c6a610452308ad1ab8bed516435432ef6094bb6804c014e13a1245e8dc62cedeb573c8d8b5952e4c1f48621e5fb75d8bbd27c15c576b3ce78537fcad200'
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
