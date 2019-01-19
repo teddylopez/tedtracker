0bdcd334ee52ae378b51cfb6598f3ca37f1649de
teddylopez <tedmlopez@gmail.com>
2019-01-19 14:20:38 -0500

    Modify application.rb for production

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require "rails"
-require 'dotenv/load'
+require "dotenv"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
