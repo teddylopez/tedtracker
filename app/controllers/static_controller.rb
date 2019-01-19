c825ffea4f40e66410ce40c0d073163a3acba943
teddylopez <tedmlopez@gmail.com>
2019-01-19 11:32:05 -0500

    Style admin homepage, create partial to display pending posts

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def homepage
-
+    @pending_approvals = Post.where(status: 'submitted')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ AdminUser.create(email: "admin@test.com",
 puts "1 admin user created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post}: Just because...", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts created"
