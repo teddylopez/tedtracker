490b2b2088840a05ebcfab76c8a879da91505e70
teddylopez <tedmlopez@gmail.com>
2019-01-19 21:41:59 -0500

    Refactor button functionality for employee dashboard

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -5,6 +5,8 @@ class AuditLog < ActiveRecord::Base
   enum status: { pending: 0, confirmed: 1 }
   before_update :set_end_date, if: :confirmed?
 
+  scope :by_start_date, -> { order('start_date DESC') }
+
   private
 
     def set_end_date
