64b8758b211575b04ea668314e46bf10b7f99e87
teddylopez <tedmlopez@gmail.com>
2019-01-20 12:23:02 -0500

    Create model relationships between managers and employees

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :audit_logs
+  has_many :hands_associations, class_name: 'Hand'
+  has_many :hands, through: :hands_associations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,4 +52,15 @@ RSpec.describe User, type: :model do
       expect(@user.full_name).to eq("LOPEZ, SHRED")
     end
   end
+
+  describe 'relationship between admins and employees' do
+    it 'allows for admins to be associated with multiple employees' do
+      employee_1 = FactoryGirl.create(:user)
+      employee_2 = FactoryGirl.create(:user)
+      admin = FactoryGirl.create(:admin_user)
+      Hand.create!(user_id: admin.id, hand_id: employee_1.id)
+      Hand.create!(user_id: admin.id, hand_id: employee_2.id)
+      expect(admin.hands.count).to eq(2)
+    end
+  end
 end
