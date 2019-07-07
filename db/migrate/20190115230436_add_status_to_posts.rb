d28c15d876befc15bf8ddfd185be38fb321bf845
teddylopez <tedmlopez@gmail.com>
2019-07-07 19:39:07 -0400

    --amend

--- db/migrate/20190115230436_add_status_to_posts.rb
+++ db/migrate/20190115230436_add_status_to_posts.rb
@@ -1,4 +1,4 @@
-class AddStatusToPosts < ActiveRecord::Migration
+class AddStatusToPosts < ActiveRecord::Migration[5.2]
   def change
     add_column :posts, :status, :integer, default: 0
   end
--- db/migrate/20190118153408_add_hours_to_posts.rb
+++ db/migrate/20190118153408_add_hours_to_posts.rb
@@ -1,4 +1,4 @@
-class AddHoursToPosts < ActiveRecord::Migration
+class AddHoursToPosts < ActiveRecord::Migration[5.2]
   def change
     add_column :posts, :overtime_request, :decimal, default: 0.0
   end
